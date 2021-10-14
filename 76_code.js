6ab8978e137c65dc1ce05bfe1aaca694f615013e
phatmo123 <butan92k55@gmail.com>
2021-10-14 16:45:34 +0700

    solve p76

---
+++ 76_code.js
@@ -0,0 +1,13 @@
+need = rest = 100; co = 0;
+function search(i, last) {
+    if (rest == 0) {
+        co++;
+        return;
+    }
+    max = last > rest ? rest : last;
+    for (var j = max; j >= 1; --j) {
+        rest -= j;
+        search(i+1, j);
+        rest += j;
+    }
+} search(0, need-1); co;
